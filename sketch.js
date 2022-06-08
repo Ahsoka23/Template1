78624a454323cc5724020ebe2b5346387de4fe58
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2022-06-08 15:18:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//declaración de las variables
+var x;
+var y;
+
+
+
+
+function setup() {
+  createCanvas(1000, 400);
+  x=200;
+  y=200;
+  //todo lo que se lee una sola vez 
+}
+
+
+
+function draw() {
+  background(220);
+ rect(x,y,10,10);
+ moveleft();
+  //funciones de bucle
+}
+
+
+function moveleft(){
+
+  }
